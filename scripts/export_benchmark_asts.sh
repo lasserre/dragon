c602561199052c6724324524565650bb04a1d153
lasserre <cstew33@gmail.com>
2026-03-01 23:04:30 -0600

    Corrected script to properly handle calling separate strip executable for arm binaries

--- scripts/export_benchmark_asts.sh
+++ scripts/export_benchmark_asts.sh
@@ -1,5 +1,5 @@
 
-NJOBS=25
+NJOBS=10
 DRAGON_DATA_FOLDER=~/dragon_data
 BENCHMARKS_FOLDER=$DRAGON_DATA_FOLDER/benchmarks
 
@@ -13,6 +13,22 @@ export_benchmark_asts () {
     exp_folder="$bm_name"_benchmark.exp
 
     orig_dir=`pwd`
+    is_armhf_O0=0
+
+    # select correct strip executable to match architecture
+    if [[ $exp_folder == *"arm64"* ]]; then
+        strip_exe="aarch64-linux-gnu-strip"
+    elif [[ $exp_folder == *"armhf"* ]]; then
+        strip_exe="arm-linux-gnueabihf-strip"
+        if [[ $exp_folder == *"O0"* ]]; then
+            is_armhf_O0=1
+        fi
+    else
+        strip_exe="strip"
+    fi
+
+    # echo $exp_folder $strip_exe $is_armhf_O0
+    # return
 
     if [ -d "./exps/$exp_folder" ]; then
         echo "$exp_folder already exists! Skipping..."
@@ -23,7 +39,7 @@ export_benchmark_asts () {
 
     # create wildebeest experiment
     cd ./exps
-    wdb create import-dataset $exp_folder -p bin_folder=$bm_folder
+    wdb create import-dataset $exp_folder -p bin_folder=$bm_folder strip_exe=$strip_exe is_armhf_O0=$is_armhf_O0
 
     # run experiment (import into Ghidra, extract ASTs and AST var data)
     cd $exp_folder
